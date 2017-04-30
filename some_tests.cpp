360c8b5486cca19466089a80ee4cb4045b32bdbb
Evert Heylen <evertheylen@gmail.com>
2017-04-30 17:49:02 +0200

    All succeeding

--- some_tests.cpp
+++ some_tests.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 TEST(Various, Fails) {
-	EXPECT_TRUE(false) << "Should fail anyway";
+	EXPECT_TRUE(true) << "Should fail anyway";
 }
 
 TEST(Various, Succeeds) {
@@ -20,5 +20,5 @@ TEST(Various, SomeTime) {
 
 TEST(Various, Exception) {
 	EXPECT_TRUE(true) << "Shouldn't fail";
-	throw 456;
+	//throw 456;
 }
