5a49985367e56bc20adc68bad5c7d3a389f2576a
Evert Heylen <evertheylen@gmail.com>
2017-04-30 16:48:34 +0200

    Better script

--- travis.sh
+++ travis.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 ./some_tests --gtest_output=xml --gtest_color=yes > gtest_output.txt 2>&1
+EXIT_CODE=$?
+
+echo "
+╔════════════════════════════════════╗
+║              Overview              ║
+╚════════════════════════════════════╝"
 ./print-gtest.py test_detail.xml
+
+
+echo "
+╔════════════════════════════════════╗
+║               Output               ║
+╚════════════════════════════════════╝"
 cat gtest_output.txt
+
+exit $EXIT_CODE
