c1347860a304acc0e36c448b04077d02a37a58a6
Evert Heylen <evertheylen@gmail.com>
2017-04-30 15:38:02 +0200

    actual first commit

---
+++ some_tests.cpp
@@ -0,0 +1,19 @@
+
+#include "gtest/gtest.h"
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+TEST(Various, Fails) {
+    EXPECT_TRUE(false) << "Should fail anyway";
+}
+
+TEST(Various, Succeeds) {
+    EXPECT_TRUE(true) << "Shouldn't fail";
+}
+
+TEST(Various, SomeTime) {
+	this_thread::sleep_for(chrono::milliseconds(20));
+    EXPECT_TRUE(true) << "Shouldn't fail";
+}
