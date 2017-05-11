fcd03b9af4b67cba6c16c1891141e39841b0765b
Evert Heylen <evertheylen@gmail.com>
2017-05-11 22:54:12 +0200

    codacy better catch this duplication or else

--- literally_the_same.cpp
+++ literally_the_same.cpp
@@ -1,19 +1,19 @@
 
-// Haha not literally, just mostly the same
+// Some comment
 
-vector<int> otherStupidFunction3() {
+vector<int> someStupidFunction() {
 	// Some comment
 	vector<int> res;
 	for (int i=0; i<50; i++) {
 		if (i%3 == 0) {
 			std::cout << "Divisible by 3";
 		} else if ((i+1)%2 == 0) {
-			std::cout << "Blablabla";
+			std::cout << "Blabla";
 		}
 		
 		auto f = [&](int& j) {
 			i += j;
-			static int k = 6;
+			static int k = 5;
 			k += i*j;
 			i -= k;
 			return k;
@@ -26,4 +26,6 @@ vector<int> otherStupidFunction3() {
 		
 		res.push_back(i%2 == 0 ? i : i*2);
 	}
+	
+	return res;
 }
